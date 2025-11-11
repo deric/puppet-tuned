e649677d5dac798f7b860475d20687f17fdaa602
Tomas Barton <barton.tomas@gmail.com>
2025-11-11 16:59:26 +0100

    fix lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,12 +4,12 @@
 class tuned::install {
   if $tuned::manage_package {
     stdlib::ensure_packages($tuned::packages, {
-      ensure => $tuned::package_ensure,
+        ensure => $tuned::package_ensure,
     })
 
     if $tuned::manage_dependencies and !empty($tuned::dependencies) {
       stdlib::ensure_packages($tuned::dependencies, {
-        ensure => $tuned::package_ensure,
+          ensure => $tuned::package_ensure,
       })
     }
   }
